b55119b86fa066310365b01a798c897591f5706a
Brian Ruslim <bpr0422@gmail.com>
2014-02-15 15:33:52 -0500

    Update user_roles.js

--- mappings/user_roles.js
+++ mappings/user_roles.js
@@ -6,7 +6,7 @@ exports.Map = function(config) {
 	var mappr = config.mappr;
 	return {
 		destination: 'user_roles',
-		source: 'nq_etl_Users',
+		source: 'etl_Users',
 		killWhere: 'orig_user_id IS NOT NULL',
 		useMerge: true,		
 		message: 'defaulted role for imported to Users',
